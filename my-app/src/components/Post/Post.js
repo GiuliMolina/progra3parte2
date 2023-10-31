b055f0f35c1114034873054c1e890d3fe159a539
Giuliana Molina <gmolina@udesa.edu.ar>
2023-10-31 10:08:52 -0300

    posteo

---
+++ my-app/src/components/Post/Post.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import {TextInput, TouchableOpacity, View, Text, StyleSheet, FlatList} from 'react-native';
+
+class Post extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            like: false
+        }
+    }
+
+    componentDidMount(){
+
+    }
+
+    render(){
+        return(
+            <View></View>
+        )
+    }
+}
+
+export default Post;
