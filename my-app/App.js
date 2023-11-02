f5e03722f7373e1cb57f0f1962a1cee383b7ed04
mpintoescalier <mpintoescalier@udesa.edu.ar>
2023-11-02 14:18:54 -0300

    header

--- my-app/App.js
+++ my-app/App.js
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import {NavigationContainer} from "@react-navigation/native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 
+import Menu from './src/components/Menu/Menu';
 import Home from "./src/screens/Home/Home"
 import Login from "./src/screens/Login/Login"
 import Register from "./src/screens/Register/Register"
@@ -13,32 +14,31 @@ export default function App() {
     <NavigationContainer style={styles.container}>
       <Stack.Navigator>
       <Stack.Screen
-        name="Home"
+        name="Homesd"
         component ={Home}
         options = {{headerShown:true}}
-        >
-        </Stack.Screen>
-
+        />
         <Stack.Screen
         name="Login"
         component ={Login}
-        options = {{headerShown:false}}
-        >
-        </Stack.Screen>
-
+        options = {{headerShown:true}}
+        />
         <Stack.Screen
         name="Register"
         component ={Register}
-        options = {{headerShown:false}}
-        >
-        </Stack.Screen>
-
+        options = {{headerShown:true}}
+        />
         <Stack.Screen
         name="FromPostear"
         component ={FromPostear}
         options = {{headerShown:false}}
-        >
-        </Stack.Screen>
+        />
+        <Stack.Screen
+        name="Menu"
+        component ={Menu}
+        options = {{headerShown:false}}
+        />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
--- my-app/src/components/Menu/Menu.js
+++ my-app/src/components/Menu/Menu.js
@@ -1,4 +1,10 @@
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from '@react-navigation/native';
+
+import Home from "../../screens/Home/Home";
+import Login from "../../screens/Login/Login";
+import Register from "../../screens/Register/Register";
+
 import react,{Component} from "react";
 import {
     TextInput,
@@ -13,15 +19,19 @@ const Tab = createBottomTabNavigator();
 class Menu extends Component{
     constructor(){
         super();
-        this.state={
-
-        };
+        this.state={};
     }
 
-    render(
-        
-
-    )
+render(){
+    return (
+          <Tab.Navigator>
+             <Tab.Screen name="Home" component={ Home } />
+             <Tab.Screen name="Profile" component={ Login } />
+             <Tab.Screen name="NewPost" component={ Register } />
+          </Tab.Navigator>
+     );
+     
+}
 }
 
 export default Menu
